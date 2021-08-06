745a8ff75291c04ea47cf7e3a49aac6d7fe16611
Máté Bálint <matebalint98@gmail.com>
2021-08-06 09:01:38 +0200

    ProcessPayment method has been introduced, StockMachine method has been refactored, SelfCheckoutController has been extended

--- SelfCheckoutAPI/SelfCheckoutAPI/Controllers/SelfCheckoutController.cs
+++ SelfCheckoutAPI/SelfCheckoutAPI/Controllers/SelfCheckoutController.cs
@@ -10,7 +10,14 @@ namespace SelfCheckoutAPI.Controllers
 {
     public class SelfCheckoutController : ApiController
     {
-        private static Dictionary<string, int> AvailableCurrency = new Dictionary<string, int>();
+        private static Dictionary<string, int> AvailableCurrency;
+        private static List<int> Money;
+
+        public SelfCheckoutController()
+        {
+            AvailableCurrency = new Dictionary<string, int>();
+            Money = new List<int>() { 5, 10, 20, 50, 100, 200, 500, 1000, 5000, 10000, 20000 };
+        }
 
         /// <summary>
         /// Gets the currently stored currencies.
@@ -41,17 +48,39 @@ namespace SelfCheckoutAPI.Controllers
         /// <returns>A 200 OK response along with the available currencies if no exception has happened, exception message otherwise.</returns>
         [HttpPost]
         [Route("api/v1/Stock")]
-        public HttpResponseMessage StockMachine([FromBody] JObject data)
+        public HttpResponseMessage StockMachine(JObject data)
         {
             HttpResponseMessage response;
             try
             {
-                string json = data.ToString(Newtonsoft.Json.Formatting.None);
+                string json = data.ToString(Formatting.None);
                 var values = JsonConvert.DeserializeObject<Dictionary<string, int>>(json);
                 DictionaryService.Merge(AvailableCurrency, values);
                 string storedItems = JsonConvert.SerializeObject(AvailableCurrency);
                 response = Request.CreateResponse(HttpStatusCode.OK, storedItems);
 
+            }
+            catch (Exception ex)
+            {
+                response = Request.CreateResponse(HttpStatusCode.BadRequest, ex.Message);
+            }
+
+            return response;
+        }
+
+        [HttpPost]
+        [Route("api/v1/Checkout")]
+        public HttpResponseMessage ProcessPayment(JObject data)
+        {
+            HttpResponseMessage response;
+
+            try
+            {
+                string inserted = data.SelectToken("inserted").ToString();
+                string price = data.SelectToken("price").ToString();
+                var values = JsonConvert.DeserializeObject<Dictionary<string, int>>(inserted);
+                DictionaryService.Merge(AvailableCurrency, values);
+                response = Request.CreateResponse(HttpStatusCode.OK, "success");
             }
             catch (Exception ex)
             {
